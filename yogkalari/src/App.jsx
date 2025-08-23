7e5870ec54cbac09cbc1230888a4b860a3f64f5a
salman221b <epsalmaan@gmail.com>
2025-08-23 13:53:45 +0530

    Navbar name changed

--- yogkalari/src/App.jsx
+++ yogkalari/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './components/NAvbar'
+import Navbar from './components/Navbar'
 import MainPage from './pages/main-page/MainPage'
 const App = () => {
   return (
